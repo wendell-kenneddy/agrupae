186823419f354762c39ed0e9ad8b8f430985a5a9
Wendell Kenneddy <wkenneddy505@gmail.com>
2026-05-05 18:05:40 -0300

    refactor(backend/domain): refactor Assignment.java to expose a static create method

--- backend/src/main/java/com/agrupae/domain/assignment/Assignment.java
+++ backend/src/main/java/com/agrupae/domain/assignment/Assignment.java
@@ -4,12 +4,10 @@ import java.time.Instant;
 import java.util.UUID;
 
 import lombok.AccessLevel;
-import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Getter;
+import lombok.NonNull;
 
-@Builder
-@AllArgsConstructor(access = AccessLevel.PRIVATE)
 @Getter
 public class Assignment {
     private final UUID id;
@@ -21,4 +19,55 @@ public class Assignment {
     private Instant dueDate;
     private Instant createdAt;
     private Instant updatedAt;
+
+
+    @Builder(access = AccessLevel.PRIVATE)
+    private Assignment(
+        @NonNull final UUID id,
+        @NonNull final UUID courseId,
+        @NonNull final String name,
+        final String description,
+        @NonNull final AssignmentFlags assignmentFlags,
+        final boolean archived,
+        @NonNull final Instant dueDate,
+        @NonNull final Instant createdAt,
+        @NonNull final Instant updatedAt
+    ) {
+        if (name.isBlank()) throw new IllegalArgumentException("Assignment name cannot be blank.");
+        if (updatedAt.isBefore(createdAt)) throw new IllegalArgumentException("Update timestamp cannot be before creation timestamp.");
+        if (dueDate.isBefore(createdAt)) throw new IllegalArgumentException("Due date cannot be before assignment creation timestamp.");
+
+        this.id = id;
+        this.courseId = courseId;
+        this.name = name;
+        this.description = description;
+        this.assignmentFlags = assignmentFlags;
+        this.archived = archived;
+        this.dueDate = dueDate;
+        this.createdAt = createdAt;
+        this.updatedAt = updatedAt;
+    }
+
+    public static Assignment create(
+        final UUID courseId,
+        final String name,
+        final String description,
+        final Instant dueDate,
+        final AssignmentFlags assignmentFlags
+    ) {
+        UUID id = UUID.randomUUID();
+        Instant now = Instant.now();
+
+        return Assignment.builder()
+        .id(id)
+        .courseId(courseId)
+        .name(name)
+        .description(description)
+        .assignmentFlags(assignmentFlags)
+        .dueDate(dueDate)
+        .archived(false)
+        .createdAt(now)
+        .updatedAt(now)
+        .build();
+    }
 }
