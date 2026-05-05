b89b8dd479c4d522cfb283c203a313359fda9766
Wendell Kenneddy <wkenneddy505@gmail.com>
2026-05-05 18:04:02 -0300

    refactor(backend/domain): refactor course related domain entities to expose static create methods

--- backend/src/main/java/com/agrupae/domain/course/Course.java
+++ backend/src/main/java/com/agrupae/domain/course/Course.java
@@ -4,15 +4,11 @@ import java.time.Instant;
 import java.util.UUID;
 
 import lombok.AccessLevel;
-import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Getter;
-import lombok.Setter;
+import lombok.NonNull;
 
-@Builder
-@AllArgsConstructor(access = AccessLevel.PRIVATE)
 @Getter
-@Setter(value = AccessLevel.PRIVATE)
 public class Course {
     private final UUID id;
     private UUID leaderId;
@@ -22,4 +18,49 @@ public class Course {
     private boolean archived;
     private Instant createdAt;
     private Instant updatedAt;
+
+    @Builder(access = AccessLevel.PRIVATE)
+    private Course(
+        @NonNull final UUID id,
+        final UUID leaderId,
+        @NonNull final String name,
+        final String description,
+        @NonNull final String inviteCode,
+        final boolean archived,
+        @NonNull final Instant createdAt,
+        @NonNull final Instant updatedAt
+    ) {
+        if (name.isBlank()) throw new IllegalArgumentException("Course name cannot be blank.");
+        if (updatedAt.isBefore(createdAt)) throw new IllegalArgumentException("Update timestamp cannot be before creation timestamp.");
+
+        this.id = id;
+        this.leaderId = leaderId;
+        this.name = name;
+        this.description = description;
+        this.inviteCode = inviteCode;
+        this.archived = archived;
+        this.createdAt = createdAt;
+        this.updatedAt = updatedAt;
+    }
+
+    public static Course create(
+        UUID leaderId,
+        String name,
+        String description
+    ) {
+        UUID id = UUID.randomUUID();
+        String inviteCode = UUID.randomUUID().toString();
+        Instant now = Instant.now();
+        
+        return Course.builder()
+        .id(id)
+        .leaderId(leaderId)
+        .name(name)
+        .description(description)
+        .inviteCode(inviteCode)
+        .archived(false)
+        .createdAt(now)
+        .updatedAt(now)
+        .build();
+    }
 }
--- backend/src/main/java/com/agrupae/domain/course/CourseArtifact.java
+++ backend/src/main/java/com/agrupae/domain/course/CourseArtifact.java
@@ -3,16 +3,62 @@ package com.agrupae.domain.course;
 import java.time.Instant;
 import java.util.UUID;
 
+import lombok.AccessLevel;
 import lombok.Builder;
 import lombok.Getter;
+import lombok.NonNull;
 
-@Builder
 @Getter
 public class CourseArtifact {
+    private UUID id;
     private UUID courseId;
     private String name;
     private String description;
     private String resourceLink;
     private Instant createdAt;
     private Instant updatedAt;
+
+    @Builder(access = AccessLevel.PRIVATE)
+    private CourseArtifact(
+        @NonNull UUID id,
+        @NonNull UUID courseId,
+        @NonNull String name,
+        String description,
+        @NonNull String resourceLink,
+        @NonNull Instant createdAt,
+        @NonNull Instant updatedAt
+    ) {
+        if (name.isBlank()) throw new IllegalArgumentException("Course artifact name cannot be blank.");
+        if (resourceLink.isBlank()) throw new IllegalArgumentException("Resource link cannot be blank.");
+        if (updatedAt.isBefore(createdAt)) throw new IllegalArgumentException("Update timestamp cannot be before creation timestamp.");
+
+        this.id = id;
+        this.courseId = courseId;
+        this.name = name;
+        this.description = description;
+        this.resourceLink = resourceLink;
+        this.createdAt = createdAt;
+        this.updatedAt = updatedAt;
+    }
+
+    public static CourseArtifact create(
+        UUID courseId,
+        String name,
+        String description,
+        String resourceLink,
+        Instant createdAt,
+        Instant updatedAt
+    ) {
+        UUID id = UUID.randomUUID();
+        Instant now = Instant.now();
+        
+        return CourseArtifact.builder()
+        .id(id)
+        .courseId(courseId)
+        .name(name)
+        .description(description)
+        .createdAt(now)
+        .updatedAt(now)
+        .build();
+    }
 }
