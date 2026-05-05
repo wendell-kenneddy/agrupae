bdf350862bc8edb814434625247cda44e1e9451e
Wendell Kenneddy <wkenneddy505@gmail.com>
2026-05-05 18:06:20 -0300

    refactor(backend/domain): refactor group related domain entities to expose static create methods

--- backend/src/main/java/com/agrupae/domain/group/GroupArtifact.java
+++ backend/src/main/java/com/agrupae/domain/group/GroupArtifact.java
@@ -1,23 +1,69 @@
 package com.agrupae.domain.group;
 
-import lombok.AllArgsConstructor;
+import lombok.AccessLevel;
 import lombok.Builder;
 import lombok.Getter;
-import lombok.Setter;
+import lombok.NonNull;
 
 import java.time.Instant;
 import java.util.UUID;
 
-@Builder
+
 @Getter
-@AllArgsConstructor
-@Setter
 public class GroupArtifact {
-    private UUID groupId;
+    private final UUID id;
+    private final UUID groupId;
     private String name;
     private String description;
     private String resourceLink;
-    private boolean privateGroup;
+    private boolean privateArtifact;
     private Instant createdAt;
     private Instant updatedAt;
+
+    @Builder(access = AccessLevel.PRIVATE)
+    private GroupArtifact(
+        @NonNull final UUID id,
+        @NonNull final UUID groupId,
+        @NonNull final String name,
+        @NonNull final String description,
+        final boolean privateArtifact,
+        @NonNull String resourceLink,
+        @NonNull Instant createdAt,
+        @NonNull Instant updatedAt
+    ) {
+        if (name.isBlank()) throw new IllegalArgumentException("Group artifact name cannot be blank.");
+        if (resourceLink.isBlank()) throw new IllegalArgumentException("Resource link cannot be blank.");
+        if (updatedAt.isBefore(createdAt)) throw new IllegalArgumentException("Update timestamp cannot be before creation timestamp.");
+
+        this.id = id;
+        this.groupId = groupId;
+        this.name = name;
+        this.description = description;
+        this.privateArtifact = privateArtifact;
+        this.resourceLink = resourceLink;
+        this.createdAt = createdAt;
+        this.updatedAt = updatedAt;
+    }
+
+    public static GroupArtifact create(
+        final UUID groupId,
+        final String name,
+        final String description,
+        final boolean privateArtifact,
+        final String resourceLink
+    ) {
+        UUID id = UUID.randomUUID();
+        Instant now = Instant.now();
+
+        return GroupArtifact.builder()
+        .id(id)
+        .groupId(groupId)
+        .name(name)
+        .description(description)
+        .privateArtifact(privateArtifact)
+        .resourceLink(resourceLink)
+        .createdAt(now)
+        .updatedAt(now)
+        .build();
+    }
 }
