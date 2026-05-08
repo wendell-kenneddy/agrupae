01c3c52f59f23ad002fc78bffed8f907405dbe97
Wendell Kenneddy <wkenneddy505@gmail.com>
2026-05-08 08:37:28 -0300

    refactor(backend/application): remove unnecessary userId argument from logout use case and service

--- backend/src/main/java/com/agrupae/application/port/in/authentication/LogoutUseCase.java
+++ backend/src/main/java/com/agrupae/application/port/in/authentication/LogoutUseCase.java
@@ -1,7 +1,5 @@
 package com.agrupae.application.port.in.authentication;
 
-import java.util.UUID;
-
 public interface LogoutUseCase {
-    void handle(UUID userId, String rawRefreshToken);
+    void handle(String rawRefreshToken);
 }
--- backend/src/main/java/com/agrupae/application/service/authentication/LogoutService.java
+++ backend/src/main/java/com/agrupae/application/service/authentication/LogoutService.java
@@ -1,29 +1,19 @@
 package com.agrupae.application.service.authentication;
 
-import java.util.UUID;
-
 import com.agrupae.application.port.in.authentication.LogoutUseCase;
 import com.agrupae.application.port.out.authentication.RefreshTokenRepository;
 import com.agrupae.application.port.out.authentication.TokenHasher;
-import com.agrupae.application.port.out.user.UserRepository;
 import com.agrupae.domain.exception.InvalidTokenException;
-import com.agrupae.domain.exception.UserNotFoundException;
 import com.agrupae.domain.refresh_token.RefreshToken;
-import com.agrupae.domain.user.User;
 
 import lombok.RequiredArgsConstructor;
 
 @RequiredArgsConstructor
 public class LogoutService implements LogoutUseCase {
-    private final UserRepository userRepository;
     private final RefreshTokenRepository refreshTokenRepository;
     private final TokenHasher tokenHasher;
 
-    public void handle(UUID userId, String rawRefreshToken) {
-        User user = this.userRepository.findById(userId);
-
-        if (user == null) throw new UserNotFoundException();
-
+    public void handle(String rawRefreshToken) {
         String refreshTokenHash = this.tokenHasher.hash(rawRefreshToken);
         RefreshToken refreshToken = this.refreshTokenRepository.findByTokenHash(refreshTokenHash);
 
